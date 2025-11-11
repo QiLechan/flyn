6b08c96916ef2443f0d830c7b8cbc25a2315a9c1
QiLechan <qilechan@outlook.com>
2025-11-11 13:23:02 +0800

    Add email validation and Toast message in SignupScreen for verification code request

--- app/src/main/java/org/yuezhikong/flyn/ui/user/SignupScreen.kt
+++ app/src/main/java/org/yuezhikong/flyn/ui/user/SignupScreen.kt
@@ -1,5 +1,6 @@
 package org.yuezhikong.flyn.ui.user
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,14 +15,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.yuezhikong.flyn.user
 
 @Preview
 @Composable
 fun SignupScreen() {
+    val context = LocalContext.current
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var verification_code by remember { mutableStateOf("") }
@@ -35,12 +39,17 @@ fun SignupScreen() {
             value = email,
             onValueChange = {},
             label = { Text("邮箱") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
             singleLine = true
         )
         Button(
             onClick = {
-
+                if (email == "") {
+                    Toast.makeText(context, "邮箱不能为空！", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    user.sendEmail_Signup_Verification_code(email)
+                }
             },
             content = {
                 Text("获取验证码")
