0bf6ec8327c44f8847da1cc73c2b2e470405fb26
QiLechan <qilechan@outlook.com>
2025-11-14 15:51:00 +0800

    Refactor HomeScreen to include HotPointCard and ServiceStaffCard components

--- app/src/main/java/org/yuezhikong/flyn/ui/home/HomeScreen.kt
+++ app/src/main/java/org/yuezhikong/flyn/ui/home/HomeScreen.kt
@@ -1,41 +1,21 @@
 package org.yuezhikong.flyn.ui.home
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
-import androidx.compose.material3.DockedSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.semantics.isTraversalGroup
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -43,7 +23,15 @@ import androidx.navigation.NavController
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController) {
-    SearchBar(navController)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        SearchBar(navController)
+        HotPointCard()
+        ServiceStaffCard()
+    }
 }
 
 @Composable
@@ -51,7 +39,7 @@ fun SearchBar(navController: NavController){
     Box(
         modifier = Modifier
             .padding(16.dp)
-            .height(35.dp)
+            .height(40.dp)
     ){
         Card(
             modifier = Modifier
@@ -67,6 +55,56 @@ fun SearchBar(navController: NavController){
                 modifier = Modifier.padding(6.dp)
             ) {
                 Icon(Icons.Default.Search, contentDescription = "搜索")
+            }
+        }
+    }
+}
+@Composable
+fun HotPointCard() {
+    Box(
+        modifier = Modifier
+            .padding(top = 0.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+            .height(200.dp)
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxSize(),
+        ){
+            Text("热门咨询TOP3", style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ServiceStaffCard() {
+    Box(
+        modifier = Modifier
+            .padding(top = 0.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+            .height(200.dp)
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxSize(),
+        ){
+            Text("服务人员", style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+
             }
         }
     }
